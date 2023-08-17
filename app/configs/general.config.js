a19c51e8d564559a8e8a707c3492c655950cd287
LAPTOP-EB1P93V6\Hieu <scottnguyen2411@gmail.com>
2023-08-17 10:31:44 +0700

    Second change

--- app/configs/general.config.js
+++ app/configs/general.config.js
@@ -1,11 +1,11 @@
 module.exports = {
     DBConnectors: {
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
-        username: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME,
-        dialect: process.env.DB_DIALECT,
+        host: process.env.DB_HOST || "db4free.net",
+        port: process.env.DB_PORT || 3306,
+        username: process.env.DB_USER || "sheeper",
+        password: process.env.DB_PASSWORD || "H.S20T.H04",
+        database: process.env.DB_NAME || "newDatabase",
+        dialect: process.env.DB_DIALECT || "mysql",
     },
     jwtAuthKey: 'abcxyz',
     tokenLoginExpiredDays:'25 days'
